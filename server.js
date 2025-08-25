23c335937e6e79df9fd036243e32ceb3a93cf987
Lime McMile <135271728+emwimcdo@users.noreply.github.com>
2025-08-25 17:59:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ io.on("connection", socket => {
     })
 })
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 server.listen(PORT, "0.0.0.0", () => {
     console.log(`Server listening at http://0.0.0.0:${PORT}`)
 })
